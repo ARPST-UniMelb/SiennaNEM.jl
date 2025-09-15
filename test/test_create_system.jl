e746e9db0a0536ef06bbdd9417b3651a974606e0
Muhammad Yasirroni <yasirroni@users.noreply.github.com>
2025-09-15 14:42:55 +1000

    support arrow; use arrow in example

--- test/test_create_system.jl
+++ test/test_create_system.jl
@@ -5,7 +5,16 @@ const PSY = PowerSystems
 
 @testset "Create System Tests" begin
     data_dir = "../data/nem12"
-    data = read_system_data_csv(data_dir)
+    data = read_system_data(data_dir)
+    create_system!(data)
+
+    @test haskey(data, "sys")
+    @test typeof(data["sys"]) == PSY.System
+end
+
+@testset "Create System Tests from Arrow" begin
+    data_dir = "../data/nem12-arrow"
+    data = read_system_data(data_dir)
     create_system!(data)
 
     @test haskey(data, "sys")
