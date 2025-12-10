03c850b3b236e5d2c7046e48ccc373b820e592e4
Muhammad Yasirroni <yasirroni@users.noreply.github.com>
2025-12-10 19:36:09 +1100

    update note

--- examples/uc_build_problem.jl
+++ examples/uc_build_problem.jl
@@ -10,17 +10,29 @@ using HiGHS
 solver = optimizer_with_attributes(HiGHS.Optimizer, "mip_rel_gap" => 0.01)
 
 # NOTE:
-#   The example `horizon = Hour(T)` here means that each study will span for T
-# hours. The `interval = Hour(I)` here means that the model only support
-# selecting `initial_time` at every I hours, starting from the minimum date in
-# `minimum(data["demand_l_ts"][!, :date])`.
-#   Ideally, we can use as wide as possible for the horizon here and as small as
-# possible interval here to support more flexible `initial_time` selection in
-# the decision model. However, higher horizon will reduce the number of time
-# slices that can be used for the rolling forecast. Selecting `schedule_horizon`
-# later in `run_decision_model_loop` with a smaller value than this `horizon`
-# will cause the last `(horizon - schedule_horizon)` hours of time series data
-# not be solved.
+#   The `horizon = Hour(T)` parameter defines the optimization window span of T
+# hours for each decision model. The `interval = Hour(I)` parameter defines the
+# sliding window shift of I hours between consecutive time slices, starting from
+# the minimum date in `minimum(data["demand_l_ts"][!, :date])`.
+#
+#   For example, with horizon=48 and interval=24:
+#   - Window 1: Hours 0-47
+#   - Window 2: Hours 24-71 (overlaps 24 hours with Window 1)
+#   - Window 3: Hours 48-95, etc.
+#
+#   When interval < horizon, windows overlap. When interval = horizon, windows
+# are sequential without overlap. When interval > horizon, there will be gaps
+# between windows.
+#
+#   Trade-offs:
+#   - Larger horizon: Longer optimization windows, better long-term decisions.
+#   - Smaller interval: More frequent re-optimization and overlapping solutions,
+#     but have more flexibility in selecting time slices that will be used, 
+#
+#   Note: In `run_decision_model_loop`, the full `horizon` is always used for
+# each optimization window. Initial conditions (generator status, storage SoC,
+# etc.) do NOT propagate between windows in the current implementation due to
+# bug in Sienna.
 
 # input variables parameters
 system_data_dir = "data/nem12/arrow"
