9738ba1881863c4a8b8618963e2974485a2b9e10
Muhammad Yasirroni <yasirroni@users.noreply.github.com>
2026-01-24 17:45:36 +1100

    split tests into multiple files

---
+++ test/test_pisp_arrow.jl
@@ -0,0 +1,27 @@
+pisp_data_dir = joinpath(nem_reliability_data_dir, "data/pisp-datasets/out-ref4006-poe10")
+include("test_run_simulation.jl")
+@testset "PISP data - Arrow" verbose = true begin
+    if isdir(pisp_data_dir)
+        arrow_dir = joinpath(pisp_data_dir, "arrow")
+        if isdir(arrow_dir)
+            schedule_names = filter(
+                name -> startswith(name, "schedule-"),
+                readdir(arrow_dir)
+            )
+            if !isempty(schedule_names)
+                _, _, timings = test_run_simulation(
+                    arrow_dir,
+                    joinpath(arrow_dir, schedule_names[1]),
+                    "arrow",
+                )
+                all_timings["PISP-Arrow"] = timings
+            else
+                @test_skip "No schedule directories found in PISP Arrow data"
+            end
+        else
+            @test_skip "PISP Arrow data directory not found"
+        end
+    else
+        @test_skip "PISP data directory not found"
+    end
+end
